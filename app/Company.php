c43b3f257550f6c37137680f8d36f74bc1379694
danil <d.prytckov@yandex.ru>
2020-07-20 18:57:51 +0500

    Init

---
+++ app/Company.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Company
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Company query()
+ * @mixin \Eloquent
+ */
+class Company extends Model
+{
+    protected $fillable = ['name', 'address', 'website', 'email'];
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
+    Route::resource('companies', 'CompaniesController', ['except' => ['create', 'edit']]);
+});
